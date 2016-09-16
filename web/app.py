901f560de72ccef920a8570e17556f80c5e277e4
Niklas Sombert <niklas@ytvwld.de>
2016-09-17 01:07:41 +0200

    web: API: Fix polling: save a client correctly.

--- web/app.py
+++ web/app.py
@@ -68,7 +68,7 @@ class App():
 			client = self._get_client(hostname)
 			try:
 				command = client["commands"].pop()
-				self._save_client(client)
+				self._save_client(hostname, client)
 				print("Sending command {0} to client {1}...".format(command, hostname))
 				return Response(jsonenc.encode(command), mimetype="text/json", status=200)
 			except IndexError:
