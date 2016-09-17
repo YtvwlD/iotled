606e4330567d7a9497924299b5eb0c594dda2783
Niklas Sombert <niklas@ytvwld.de>
2016-09-17 02:35:02 +0200

    web: Add manage.html.

---
+++ web/static/js/manage.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var devices = [];
+
+function listDevices()
+{
+	$.ajax({
+		url: "/api/app/list",
+		type: "GET",
+		dataType: "json",
+		success: function (json)
+		{
+			json.forEach(function(device)
+			{
+				devices.append(device);
+				$("<tr></tr>")
+					.append($("<td></td>")
+						.html(device)
+					)
+					.appendTo("#devices-list");
+			});
+		},
+		error: function(xhr, status, errorThrown)
+		{
+			alert("Sorry, there was a problem.");
+			console.log("Error: " + errorThrown);
+			console.log("Status: " + status);
+			console.dir(xhr);
+		}
+	});
+}
+
+listDevices();
