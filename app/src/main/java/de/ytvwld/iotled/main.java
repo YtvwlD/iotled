0f453e89485dbb68f783be87f63cb784f351b9da
Niklas Sombert <niklas@ytvwld.de>
2016-09-17 10:29:30 +0200

    Add an Android app.

---
+++ app/src/main/java/de/ytvwld/iotled/main.java
@@ -0,0 +1,15 @@
+package de.ytvwld.iotled;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
